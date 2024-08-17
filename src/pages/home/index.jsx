8cd1f2f8ced7ee7813d096cf8167815e2803a8e4
kasranajafii <kasranajafi40@gmail.com>
2024-08-17 16:05:41 +0330

    adding status

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,11 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import AnimatedView from "./components/animatedView";
 import "./home.scss";
 
 const Home = () => {
+    const [cityInfo, setCityInfo] = useState({});
+    const [weatherInfo, setWeatherInfo] = useState({});
+    const [unit, setUnit] = useState("imperial");
     return (
         <div className="home">
             <AnimatedView />
+            <Status cityInfo={cityInfo} weatherInfo={weatherInfo} unit={unit} />
         </div>
     );
 };
