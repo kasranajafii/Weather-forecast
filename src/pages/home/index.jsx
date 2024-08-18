87bdb1fadeb3ee2f9d6155c916ad57dc1780616e
kasranajafii <kasranajafi40@gmail.com>
2024-08-18 12:11:02 +0330

    sidebar added to my project

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,13 +1,24 @@
 import React, { useState } from "react";
 import AnimatedView from "./components/animatedView";
 import "./home.scss";
+import SideBar from "../../components/sideBar";
 
 const Home = () => {
     const [cityInfo, setCityInfo] = useState({});
     const [weatherInfo, setWeatherInfo] = useState({});
+    const [searchTerm, setSearchTerm] = useState("New York");
+    const [sideBarStatus, setSideBarStatus] = useState(false);
     const [unit, setUnit] = useState("imperial");
     return (
         <div className="home">
+            <SideBar
+                isOpen={sideBarStatus}
+                onClose={() => setSideBarStatus(false)}
+                onInput={(e) => setSearchTerm(e.target.value)}
+                searchTerm={searchTerm}
+                onUnit={(e) => setUnit(e.target.value)}
+                unit={unit}
+            />
             <AnimatedView />
             <Status cityInfo={cityInfo} weatherInfo={weatherInfo} unit={unit} />
         </div>
